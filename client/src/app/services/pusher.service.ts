d353ed57bc5e2f766f24a086c4b2f89c492b7bcb
skrishan7 <53896939+skrishan7@users.noreply.github.com>
2020-05-17 21:26:20 +1000

    env stuff

---
+++ client/src/app/services/pusher.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+@Injectable()
+export class PusherService {
+  channel;
+  baseURL = 'http://localhost:3000/api/';
+  
+  constructor(private http: HttpClient) {
+    const pusher = new Pusher('1043d15335fe067e14c8', {
+      cluster: 'ap1',
+    });
+    this.channel = pusher.subscribe('painting');
+  }
+  
+  public init() {
+    return this.channel;
+  }
+
+  makeRequest(enco, id) {
+    const headers = new HttpHeaders();
+    headers.append('Content-Type', 'application/json');
+    return this.http.post(this.baseURL + 'uml', {
+        encoded: enco,
+        userId: id,
+      }, { headers });
+  }
+}
